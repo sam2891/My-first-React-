5eb5c61e71f0b8d986e6e1390fb86b1dd0a29748
sam2891 <dwsnsamuel@gmail.com>
2023-08-04 16:18:55 +0530

    React File

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './component/Home ';
+
+function App() {
+  const heading = "My First React Application"
+
+  return (
+    <div className="Sam">
+      <Home title={heading} />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Home .js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Home({ title }) {
+
+
+    return (
+
+        <div>
+            <h1>
+                {title}
+            </h1>
+
+            <h2>About Me:</h2>
+            <div>
+                <img src="https://picsum.photos/200/300" alt="random"></img>
+            </div>
+            <p>
+                svyystvynaptvmypatvmitc,povohxgk.hgs;rhg;hre;wonbononb;on;obn;jn;osrn:ONbno"Obn;ronzobn[rfndmon
+                bdvsbivbpipionvdpiobnpsojBNOPNBO:NBPBpoNBPONBponob<br></br>
+                vsbibvibvdisbivbibvpibvibibnibnfanbnlbn;zfbn;bhnbcijhanfbpnfobonpbnpbnzpbiznabpainbPIONBPnbpnbfpbfn
+                seagingnhnaphondponmpoketobm....
+            </p>
+
+
+        </div>
+    )
+}
+
+export default Home 
