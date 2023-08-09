fc39f9e2a549d0d72bcf4b59bb89fb2f93de0722
sam2891 <dwsnsamuel@gmail.com>
2023-08-09 12:45:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
 import './App.css';
+import Array from './component/Array';
 import Home from './component/Home ';
 
 function App() {
   const heading = "My First React Application"
+  // eslint-disable-next-line no-unused-vars
+  const fruits = ["Apple","Banana","Orange","Mango"]
+  const allow = true
+
+  
 
   return (
     <div className="Sam">
       <Home title={heading} />
+      <h1>My Fruits</h1>
+      <Array fruits={fruits} />
+
+      
+       
+
+
+      {/*<ul>
+        {    //java script canbe written in HTml with curly brackets 
+
+        fruits.map((fruits,index)=> (
+        <li key= {index}>{fruits}</li>
+         ))
+         }
+        </ul>*/}
 
 
     </div>
